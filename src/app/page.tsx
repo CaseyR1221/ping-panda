b02f20b5f493e2a33d1d4541ac754a29e7e88cc2
Casey Rowlands <crowlandsdev@gmail.com>
2025-11-08 15:22:34 -0500

    created the max-width-wrapper and heading components for standardization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+import  {MaxWidthWrapper } from "@/app/components/max-width-wrapper"
+import { Heading } from "./components/heading"
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="text-center">
+          <div className="relative flex flex-col items-center text-center mx-auto gap-10">
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights,</span>
+                <br />
+                <span>Delivered to Your Discord</span>
+              </Heading>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section>2</section>
+      <section>3</section>
+      <section>4</section>
+      <section>5</section>
+    </>
   )
 }
+
 export default Homepage
