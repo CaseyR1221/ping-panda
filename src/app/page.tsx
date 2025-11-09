07d91afe12ab4cd68726e873beee900ebf7adf86
Casey Rowlands <crowlandsdev@gmail.com>
2025-11-09 15:56:10 -0500

    added in brand colors, and added text content to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import  {MaxWidthWrapper } from "@/app/components/max-width-wrapper"
+import { MaxWidthWrapper } from "@/app/components/max-width-wrapper"
 import { Heading } from "./components/heading"
+import { Check } from "lucide-react"
 
 const Homepage = () => {
   return (
@@ -11,9 +12,29 @@ const Homepage = () => {
               <Heading>
                 <span>Real-Time SaaS Insights,</span>
                 <br />
-                <span>Delivered to Your Discord</span>
+                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Delivered to Your Discord
+                </span>
               </Heading>
             </div>
+
+            <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+              PingPanda is the easiest way to monitor your SaaS. Get instant
+              notifications for{" "}
+              <span className="font-semibold text-gray-700">
+                sales, new users, or any other event
+              </span>{" "}
+              sent directly to your discord.
+            </p>
+
+            <ul className="flex flex-col items-center sm:items-start space-y-2 text-base/7 text-gray-600">
+              {["Real-time Discord Alerts for critical moments", "Buy once, use forever", "Track sales, signups, and more"].map((item, index) => (
+                <li key={index} className="flex gap-1.5 items-center text-left">
+                  <Check className="size-5 shrink-0 text-brand-700" />
+                  {item}
+                </li>
+              ))}
+            </ul>
           </div>
         </MaxWidthWrapper>
       </section>
